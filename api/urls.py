3959de479fe2700ae541a59d55592ff5d4aa4a29
nikunj <nikunjagarwal1704@outlook.com>
2026-06-30 15:32:23 +0900

    step  3

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
 from api.views.task_views import TaskListView, TaskDetailView
+from api.views.user_views import UserSearchView
 
 urlpatterns = [
     path('tasks', TaskListView.as_view()),
     path('tasks/<int:pk>', TaskDetailView.as_view()),
+    path('users/search', UserSearchView.as_view()),
 ]
--- api/views/user_views.py
+++ api/views/user_views.py
@@ -6,37 +6,53 @@ from django.views import View
 from django.views.decorators.csrf import csrf_exempt
 
 from api.models import User
-from api.serializers import UserSerializer
 
 
 @method_decorator(csrf_exempt, name='dispatch')
 class UserSearchView(View):
-  def post(self, request):
-    # TODO: Step3 - Search the users table with the conditions (name / email / minAge / maxAge)
-    #               from the request body using AND conditions, and return the results
-    #
-    # Hint:
-    #   data = json.loads(request.body)
-    #   qs = User.objects.all()
-    #   if data.get('name'):
-    #       qs = qs.filter(name__icontains=data['name'])
-    #   ...
-    return JsonResponse({}, status=404)
+    def post(self, request):
+        try:
+            data = json.loads(request.body) if request.body else {}
+        except (json.JSONDecodeError, TypeError):
+            return JsonResponse({'error': 'Invalid JSON.'}, status=400)
+
+        qs = User.objects.prefetch_related('orders').all()
+
+        if data.get('name'):
+            qs = qs.filter(name__icontains=data['name'])
+        if data.get('email'):
+            qs = qs.filter(email__icontains=data['email'])
+        if data.get('minAge') is not None:
+            qs = qs.filter(age__gte=data['minAge'])
+        if data.get('maxAge') is not None:
+            qs = qs.filter(age__lte=data['maxAge'])
+
+        result = []
+        for user in qs:
+            result.append({
+                'id': user.id,
+                'name': user.name,
+                'email': user.email,
+                'age': user.age,
+                'orders': [{'id': o.id, 'itemName': o.item_name} for o in user.orders.all()],
+            })
+
+        return JsonResponse(result, safe=False, status=200)
 
 
 @method_decorator(csrf_exempt, name='dispatch')
 class UserOrdersView(View):
-  def get(self, request):
-    # TODO: Step4 - The implementation below has an N+1 problem. Resolve it using prefetch_related
-    users = User.objects.all()
-    result = []
-    for user in users:
-      orders = [{'id': o.id, 'itemName': o.item_name} for o in user.orders.all()]
-      result.append({
-        'id': user.id,
-        'name': user.name,
-        'email': user.email,
-        'age': user.age,
-        'orders': orders,
-      })
-    return JsonResponse(result, safe=False)
+    def get(self, request):
+        # TODO: Step4 - The implementation below has an N+1 problem. Resolve it using prefetch_related
+        users = User.objects.all()
+        result = []
+        for user in users:
+            orders = [{'id': o.id, 'itemName': o.item_name} for o in user.orders.all()]
+            result.append({
+                'id': user.id,
+                'name': user.name,
+                'email': user.email,
+                'age': user.age,
+                'orders': orders,
+            })
+        return JsonResponse(result, safe=False)
